8d4e88bd1f9a0271eb4df07a826a42623cfbec79
Ly <lxn2@uw.edu>
2013-02-11 17:24:32 -0800

    Role create table

--- Project/SQLQuery.sql
+++ Project/SQLQuery.sql
@@ -1,12 +1,19 @@
-create table PERSON (
+create table ROLE (
 	ID			integer identity(1,1),
-	LNAME			varchar(40) not null,
+	NAME			varchar(12) not null,
 	FNAME			varchar(40) not null,
 	EMAIL			varchar(60),
 	PHONE_AC		char(3),
 	PHONE			char(8) default(999-9999),
 	PASS_WORD		varchar(20) not null,
 	ROLE_ID		integer not null,
+	constraint PERSON_ID_PK primary key(ID)
+)
+go
+
+create table ROLE (
+	ID			integer identity(1,1),
+	NAME			varchar(12) not null,
 	constraint PERSON_ID_PK primary key(ID)
 )
 go
