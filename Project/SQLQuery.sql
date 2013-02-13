660d7765dfa0af42c7dbb8471bcde5e7f6d6f408
Ly <lxn2@uw.edu>
2013-02-12 17:04:44 -0800

    create tables updated

--- Project/SQLQuery.sql
+++ Project/SQLQuery.sql
@@ -1,4 +1,4 @@
-create table PERSON (
+create table FP.PERSON (
 	ID			integer identity(1,1),
 	NAME			varchar(12) not null,
 	FNAME			varchar(40) not null,
@@ -7,23 +7,93 @@ create table PERSON (
 	PHONE			char(8) default(999-9999),
 	PASS_WORD		varchar(20) not null,
 	ROLE_ID		integer not null,
-	constraint PERSON_ID_PK primary key(ID)
+	constraint FP.PERSON_ID_PK primary key(ID)
 )
 go
 
-create table ROLE (
+
+
+
+
+
+
+create table FP.ROLE (
 	ID			integer identity(1,1),
 	NAME			varchar(12) not null,
-	constraint ROLE_ID_PK primary key(ID)
+	constraint FP.ROLE_ID_PK primary key(ID)
 )
 go
 
-class table CLASS (
+
+
+
+
+
+
+create table FP.CLASS (
     ID              integer identity(1,1),
     ROOM            varchar(40),
     GRADE_LEVEL_ID  integer not null,
     TERM_ID         integer null,
     MAIN_TEACHER    integer not null,
-    constraint CLASS_GRADE_LEVEL_ID_FK foreign key(GRADE_LEVEL_ID) references Grade(ID)
+    constraint FP.CLASS_ID_PK primary key(ID)
 )
 go
+
+alter table FP.CLASS
+add constraint FP.CLASS_GRADE_LEVEL_ID_FK 
+foreign key(GRADE_LEVEL_ID) 
+references FP.GRADE(ID)
+
+alter table FP.CLASS
+add constraint FP.CLASS_TERM_ID_FK
+foreign key(TERM_ID)
+references FP.TERM(ID)
+
+alter table FP.CLASS
+add constraint FP.CLASS_MAIN_TEACHER_FK
+foreign key(MAIN_TEACHER)
+references FP.PERSON(ID)
+
+
+
+
+
+
+
+create table FP.STUDENT (
+    ID              integer identity(1,1),
+	NAME			varchar(40) not null,
+	FNAME			varchar(40) not null,
+	EMAIL			varchar(60) null,
+	PHONE_AC		char(3) null,
+	PHONE			char(8) default(999-9999),
+	CURRENT_CASS	integer null,
+	CASE_WORKER		integer not null,
+    constraint FP.STUDENT_ID_PK primary key(ID)
+)
+go
+
+alter table FP.STUDENT
+add constraint FP.STUDENT_CURRENT_CLASS_FK
+foreign key(CURRENT_CLASS)
+references FP.CLASS(ID)
+
+alter table FP.STUDENT
+add constraint FP.STUDENT_CASE_WORKER_FK
+foreign key(CASE_WORKER)
+references FP.PERSON(ID)
+
+
+
+
+
+
+
+create table FP.GRADE (
+    ID              integer identity)1,1),
+    NAME            varchar(12) not null,
+    constraint FP.GRADE_ID_PK primary key(ID)
+)
+go
+
